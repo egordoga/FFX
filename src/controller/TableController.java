13091b4c6b16340946064c3ffd2c8224c43591ae
egordoga <edoga@ukr.net>
2017-06-19 21:33:09 +0300

    firstWorked

--- src/controller/TableController.java
+++ src/controller/TableController.java
@@ -4,18 +4,15 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
+import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import model.MyFile;
-import model.MyTab;
 import sample.SimpleFileTreeItem;
-
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -25,73 +22,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class TableController extends TableView{
+public class TableController extends TableView {
 
-   /* @FXML
-    TableView<MyFile> tableFile;*/
-    @FXML
-    TableColumn<MyFile, File> colName;
-    @FXML
-    TableColumn<MyFile, String> colDateModif;
-    @FXML
-    TableColumn<MyFile, String> colSize;
     @FXML
     TreeView<File> paneTree;
     @FXML
     AnchorPane anchorTree;
     @FXML
-    Button btnUp;
-    @FXML
     ChoiceBox<File> cbDrive;
     @FXML
     ToolBar tb;
-    /*@FXML
-    Tab tab;*/
     @FXML
     AnchorPane anchTab;
 
 
-    String path = "d:\\";
+    private String path = "d:\\";
     private MyFile myFile = new MyFile();
-    private int idTab = 1;
     private Map<String, File> mapTab = new HashMap<>();
-    File dir = new File(path);
-    File file;
+    private File dir = new File(path);
+    private File file;
+    private TabPane tabPane = new TabPane();
 
-
-    Tab tab = new Tab();
-    TabPane tabPane = new TabPane();
-    TableView<MyFile> tableFile;
 
     @FXML
-    private void initialize(){
+    private void initialize() {
 
         addTab(dir);
-
-        //tableFile = addTable(dir);
         initializeTabPane();
-        //initializeTable();
-
-        //tab.setContent(tableFile);
-
         initializeTree(dir);
-
         initializeChoiceBox();
-
-        //tabListener();
     }
 
     private void initializeTree(File f) {
 
-        paneTree = new TreeView<File>(
+        paneTree = new TreeView<>(
                 new SimpleFileTreeItem(f));
         paneTree.setCellFactory(param -> new TreeController.AttachmentListCell());
         paneTree.getRoot().setExpanded(true);
         anchorTree.getChildren().add(paneTree);
         paneTree.getSelectionModel().selectedItemProperty().addListener(
                 (e, a, b) -> {
-                    //dir = b.getValue();
-                    //tableFile.setItems(myFile.getList(dir));
                     TableView<MyFile> tv = (TableView<MyFile>) tabPane.getSelectionModel().getSelectedItem().getContent();
                     tv.setItems(myFile.getList(b.getValue()));
                     tabName(b.getValue());
@@ -99,13 +69,6 @@ public class TableController extends TableView{
                 });
     }
 
-    private void initializeTable() {
-
-
-
-
-
-    }
 
     private TableView<MyFile> addTable(File dir) {
         TableView<MyFile> tableFile1 = new TableView<>();
@@ -121,14 +84,11 @@ public class TableController extends TableView{
         tableFile1.getColumns().addAll(colName, colDateModif, colSize);
         tableFile1.setItems(myFile.getList(dir));
 
-        System.out.println(dir.getParent() + "kkkkkkkk");
-
         File[] d = {dir};
-
-        tableFile1.setRowFactory( tv -> {
+        tableFile1.setRowFactory(tv -> {
             TableRow<MyFile> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
-                if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
                     MyFile rowData = row.getItem();
 
                     if (rowData.getFi().isDirectory()) {
@@ -147,35 +107,29 @@ public class TableController extends TableView{
                             tabName(d[0]);
                             mapTab.put(tabPane.getSelectionModel().getSelectedItem().getId(), d[0]);
                         }
-                    }
-                    catch (Exception ee) {
+                    } catch (Exception ee) {
                         System.out.println(ee);
                     }
                 }
             });
-            return row ;
+            return row;
         });
 
-        dir = d[0];
-        System.out.println(dir.getParent() + "llllll");
-
+        //dir = d[0];
         return tableFile1;
     }
 
-    private void initializeChoiceBox(){
+    private void initializeChoiceBox() {
         ObservableList<File> listDrive = FXCollections.observableArrayList();
         File[] roots = File.listRoots();
         Arrays.stream(roots).forEach(listDrive::add);
-        //listDrive.forEach(System.out::println);
         cbDrive.setItems(listDrive);
         cbDrive.setAccessibleText(roots[0].getAbsolutePath());
         cbDrive.getSelectionModel().selectedItemProperty().addListener(
-                (ObservableValue<? extends File> ov, File oldVal, File newVal) ->initializeTree(newVal));
+                (ObservableValue<? extends File> ov, File oldVal, File newVal) -> initializeTree(newVal));
     }
 
-    private void initializeTabPane(){
-
-        //addTab(dir);
+    private void initializeTabPane() {
         anchTab.getChildren().add(tabPane);
         AnchorPane.setBottomAnchor(tabPane, 5.0);
         AnchorPane.setLeftAnchor(tabPane, 5.0);
@@ -183,34 +137,24 @@ public class TableController extends TableView{
         AnchorPane.setTopAnchor(tabPane, 5.0);
     }
 
-    /*private void addTab(TabPane tabPane) {
-
-        Tab tab1 = new Tab(dir.getAbsolutePath());
-        tab.setText(dir.getAbsolutePath());
-        idTab++;
-        mapTab.put(idTab, dir);
-        tabPane.getTabs().add(tab1);
-    }*/
 
     @FXML
-    private void onClickBtnUp(){
+    private void onClickBtnUp() {
 
-       File dir = mapTab.get(tabPane.getSelectionModel().getSelectedItem().getId());
-       if (dir.isDirectory()){
-          if (!(dir.getParent() == null)) {
-             // dir = new File(dir.getParent());
-              dir = dir.getParentFile();
-              TableView<MyFile> tv = (TableView<MyFile>) tabPane.getSelectionModel().getSelectedItem().getContent();
-              tv.setItems(myFile.getList(dir));
-              //addTable(dir);
-              tabName(dir);
-              mapTab.put(tabPane.getSelectionModel().getSelectedItem().getId(), dir);
-          }
-       }
+        File dir = mapTab.get(tabPane.getSelectionModel().getSelectedItem().getId());
+        if (dir.isDirectory()) {
+            if (!(dir.getParent() == null)) {
+                dir = dir.getParentFile();
+                TableView<MyFile> tv = (TableView<MyFile>) tabPane.getSelectionModel().getSelectedItem().getContent();
+                tv.setItems(myFile.getList(dir));
+                tabName(dir);
+                mapTab.put(tabPane.getSelectionModel().getSelectedItem().getId(), dir);
+            }
+        }
+        System.out.println(mapTab.size());
     }
 
     private void tabName(File dir) {
-        //File dir = mapTab.get(tabPane.getSelectionModel().getSelectedItem().getId());
         if (!(dir.getParent() == null)) {
             tabPane.getSelectionModel().getSelectedItem().setText(dir.getName());
         } else {
@@ -219,34 +163,10 @@ public class TableController extends TableView{
     }
 
 
-
-   /* @FXML
-    public void addTab(File dir) {
-
-        Tab tab = new Tab(dir.getName());
-        tab.setId(String.valueOf(tab.hashCode()));
-        mapTab.put(tab.getId(), dir);
-        tab.setContent(addTable(dir));
-        tabPane.getTabs().add(tab);
-        System.out.println(tabPane.getSelectionModel().getSelectedItem());
-
-         File prev = mapTab.get(tabPane.getSelectionModel().getSelectedItem().getId());
-       File next = prev.getParentFile();
-        TableView<MyFile> tv = (TableView<MyFile>) tabPane.getSelectionModel().getSelectedItem().getContent();
-        tv.setItems(myFile.getList(next));
-        mapTab.put(tabPane.getSelectionModel().getSelectedItem().getId(), next);
-    }*/
-
-    private void tabListener(){
-       // tabPane.getSelectionModel().getSelectedItem().setText("HHHHHHH");
-
-    }
-
     @FXML
     private void addTab(File dir) {
         Tab tab = new Tab();
-        MyTab myTab = new MyTab(tab, dir);
-        if (dir.getParent() == null){
+        if (dir.getParent() == null) {
             tab.setText(dir.getAbsolutePath());
         } else {
             tab.setText(dir.getName());
@@ -255,14 +175,15 @@ public class TableController extends TableView{
         mapTab.put(tab.getId(), dir);
         tab.setContent(addTable(dir));
         tabPane.getTabs().add(tab);
-
-        //return myTab;
+        tab.setOnClosed(event ->
+        mapTab.remove(tab.getId())
+        );
     }
 
     public void clickAddTab(ActionEvent actionEvent) {
-       addTab(mapTab.get(tabPane.getSelectionModel().getSelectedItem().getId()));
-    }
+        addTab(mapTab.get(tabPane.getSelectionModel().getSelectedItem().getId()));
 
+    }
 
     public static class AttachmentTableCell extends TableCell<MyFile, File> {
         @Override
@@ -289,13 +210,8 @@ public class TableController extends TableView{
     }
 
     public static javax.swing.Icon getJSwingIconFromFileSystem(File file) {
-
         FileSystemView view = FileSystemView.getFileSystemView();
         javax.swing.Icon icon = view.getSystemIcon(file);
-
         return icon;
     }
-
-
-
 }
